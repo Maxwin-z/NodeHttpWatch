b43dd9d338277a8aa2b7955dabd313f9446f379d
Maxwin-z <zwd2005@gmail.com>
2015-06-01 13:59:28 +0800

    add express

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 var colors = require('colors');
+var express = require('express');
+var app = express();
 
-console.log('server start'.green);
+app.use(express.static('public'));
+
+app.all('*', function (req, res) {
+	console.log(req.url);
+	res.send('Hello world');
+});
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('servre start'.green, host + ':' + port);
+});
+
+
 
