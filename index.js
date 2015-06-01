be8214e830685b36a0e3def4c8b243f7c3c571be
Maxwin-z <zwd2005@gmail.com>
2015-06-01 18:15:56 +0800

    add proxy mode

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
+var URL = require('url');
+var http = require('http');
 var colors = require('colors');
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 
-app.all('*', function (req, res) {
-	console.log(req.url);
-	res.send('Hello world');
+app.all('*', function(request, response) {
+    console.log(request.connection.remoteAddress);
+    var url = request.url;
+    var opts = URL.parse(url);
+    opts.headers = request.headers;
+    opts.method = request.method;
+
+    console.log('proxy:', request.method, url);
+    // console.log(opts);
+    var proxyRequest = http.request(opts, function(proxyResponse) {
+        response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+        proxyResponse.on('data', function(chunk) {
+            response.write(chunk, 'binary');
+        });
+        proxyResponse.on('end', function(chunk) {
+            response.end(chunk || null);
+        });
+    });
+
+    proxyRequest.on('error', function() {
+        console.log('proxyRequest.error', arguments);
+    });
+
+    request.on('data', function(chunk) {
+        proxyRequest.write(chunk);
+    });
+    request.on('end', function(chunk) {
+        proxyRequest.end(chunk || null);
+    });
 });
 
-var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-	console.log('servre start'.green, host + ':' + port);
+    console.log('servre start'.green, host + ':' + port);
 });
-
-
-
