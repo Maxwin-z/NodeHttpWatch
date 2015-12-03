3c4a11ae539e90d323d5a5a745462074805853a5
Maxwin-z <zwd2005@gmail.com>
2015-12-03 15:25:04 +0800

    add index.html

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,10 @@
-console.log('index.js');
+function loop() {
+    $.getJSON('/loop', function(data) {
+        var code = $('<code>');
+        code.append(JSON.stringify(data, true, 4));
+        $('#logs').append(code);
+        setTimeout(loop, 1);
+    });
+}
+
+loop();
