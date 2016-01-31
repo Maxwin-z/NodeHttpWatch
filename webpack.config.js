241bc89b88c22cd357848ec93ff6daa1427b0e7c
Maxwin <zwd2005@gmail.com>
2016-01-31 10:18:48 +0800

    remove bower componments version

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ var providePlugin = new webpack.ProvidePlugin({
 });
 
 var context = path.join(__dirname, '/');
-var jqueryPath = path.join(context, '/bower_components/jquery/jquery-2.1.4/dist/jquery.js');
-var underscorePath = path.join(context, '/bower_components/underscore/underscore-1.8.3/underscore.js');
+var jqueryPath = path.join(context, '/bower_components/jquery/dist/jquery.js');
+var underscorePath = path.join(context, '/bower_components/underscore/underscore.js');
 var bootstrapPath = path.join(context, '/bower_components/bootstrap/dist/js/bootstrap.js');
 var reactPrefix = path.join(context, '/bower_components/react/react-bower-0.14.3/');
 
